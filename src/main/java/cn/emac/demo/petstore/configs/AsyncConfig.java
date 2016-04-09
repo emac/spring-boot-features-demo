7ca019715ec9c69f3c85c6439b31c71a7d9adca7
Emac <shen.bin.1983@gmail.com>
2016-04-09 20:22:38 +0800

    add async sample

---
+++ src/main/java/cn/emac/demo/petstore/configs/AsyncConfig.java
@@ -0,0 +1,33 @@
+package cn.emac.demo.petstore.configs;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author Emac
+ * @since 2016-04-09
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(15);
+        executor.setQueueCapacity(30);
+        executor.setMaxPoolSize(30);
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+}
--- src/main/java/cn/emac/demo/petstore/controllers/IndexController.java
+++ src/main/java/cn/emac/demo/petstore/controllers/IndexController.java
@@ -7,11 +7,17 @@ import cn.emac.demo.petstore.services.SignonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
@@ -50,5 +56,39 @@ public class IndexController {
         LinkedPage<Signon> page = new LinkedPage<>(pageBuilder.build(), "/page");
         model.addAttribute("page", page);
         return "page";
+    }
+
+    @RequestMapping(value = "/async", method = RequestMethod.GET)
+    @ResponseBody
+    public DeferredResult<String> async() {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        _async(1000).addCallback(new ListenableFutureCallback<String>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                deferredResult.setErrorResult(ex);
+            }
+
+            @Override
+            public void onSuccess(String result) {
+                deferredResult.setResult(result);
+            }
+        });
+        return deferredResult;
+    }
+
+    @RequestMapping(value = "/async2", method = RequestMethod.GET)
+    @ResponseBody
+    public ListenableFuture<String> async2() {
+        return _async(2000);
+    }
+
+    @Async
+    public ListenableFuture<String> _async(long delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        return AsyncResult.forValue("async");
     }
 }
