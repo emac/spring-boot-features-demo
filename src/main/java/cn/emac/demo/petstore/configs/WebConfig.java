98b438616666de300ae82c652902baa387531194
Emac <shen.bin.1983@gmail.com>
2017-08-05 22:43:09 +0800

    enable spring cache busting

--- src/main/java/cn/emac/demo/petstore/configs/WebConfig.java
+++ src/main/java/cn/emac/demo/petstore/configs/WebConfig.java
@@ -17,6 +17,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -31,18 +32,11 @@ import static cn.emac.demo.petstore.PetstoreConstants.ENUM_PROP_NAME;
 @ServletComponentScan("cn.emac")
 @EnableSpringDataWebSupport
 @EnableRetry
+@EnableScheduling
 public class WebConfig extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer {
 
     @Value("${server.error.path:${error.path:/error}}")
     private String errorPath = "/error";
-
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        // 启用方法层面(参数,返回值)的校验,跟@Validated注解配合使用
-        MethodValidationPostProcessor processor = new MethodValidationPostProcessor();
-        processor.setProxyTargetClass(true);
-        return processor;
-    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -56,18 +50,26 @@ public class WebConfig extends WebMvcConfigurerAdapter implements EmbeddedServle
         configurer.setTaskExecutor(SpringExecutorFactory.build("default-DeferredResult", 15, 30, 30));
     }
 
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer factory) {
+        // highest priority: fine-grained error control for each HTTP status
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, errorPath));
+        factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, errorPath));
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        // 启用方法层面(参数,返回值)的校验,跟@Validated注解配合使用
+        MethodValidationPostProcessor processor = new MethodValidationPostProcessor();
+        processor.setProxyTargetClass(true);
+        return processor;
+    }
+
     @Bean
     public MyErrorController errorController() {
         // uniform error control
         ErrorAttributes errorAttributes = new DefaultErrorAttributes();
         ErrorProperties errorProperties = new ErrorProperties();
         return new MyErrorController(errorAttributes, errorProperties);
-    }
-
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer factory) {
-        // highest priority: fine-grained error control for each HTTP status
-        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, errorPath));
-        factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, errorPath));
     }
 }
